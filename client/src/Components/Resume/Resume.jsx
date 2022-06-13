13cf2c5eeb5302f742fadab6c6256af27230c465
kthousand <kthousand@jacapps.com>
2022-06-13 19:05:09 -0400

    updated resume

--- client/src/Components/Resume/Resume.jsx
+++ client/src/Components/Resume/Resume.jsx
@@ -10,29 +10,31 @@ function Resume(props) {
           <br />
           <br />
           <p>
-            Starting with a passion for aesthetic design and artwork, my love of
-            functionality and structure evolved into my full-stack development
-            career. My working method comes from my service-industry experience;
-            a place of approachability, creativity, and calm under pressure. My
-            success has come through my innate ambition, attention to detail,
-            and desire to communicate my efforts to ensure efficiency and
-            viability.
+            Starting with a passion for aesthetic design and artwork, my love of functionality and structure evolved into my web development career. My working method comes from a place of approachability, creativity, and calm under pressure. My success has come through my innate ambition, attention to detail, and my commitment to continue refining my skills through learning.
           </p>
           <br />
           <h2>Technical Skills</h2>
           <h3>Software Development</h3>
           <br />
           <p>
-            React, JavaScript, CSS, HTML, Github, Node.js, Express, SQL,
-            MongoDB, Python.
-          </p>
-          <h3>Graphic Design</h3>
-          <br />
-          <p>
-            Adobe Creative Suite, specializing in Adobe Illustrator, Photoshop,
-            and InDesign.
+            JavaScript, Typescript, CSS, SASS, SQL, HTML, Python, Express, Node, Ruby on Rails <br />
+React, MaterialUI, Vue, Vuetify <br /><br />
+TypeORM, Sequelize, Prisma, MongoDB, Jest, Debounce, ESLint, Prettier, Axios, Firebase, GoogleMapsAPI, Keycloak, JSONWebToken, Bcrypt, Winston <br /><br />
+Jira, Airtable, Asana,inVision, Toggl, Adobe Creative Suite
           </p>
           <h2>Experience and Education</h2>
+          <h3>Jācapps</h3>
+          <h4>Full Stack Web Developer</h4>
+          <br />
+          <p>
+            Member of a small web development team building applications from the ground up. Participated in planning and strategizing web app development with Agile and Waterfall methodology.
+            <br />
+            <br />
+            Involved in the plotting of features with time and priority estimation, utilizing Jira Kanban boards and Roadmaps, as well as Airtable communication. Cooperation with UX/UI team in development of feature-based design.
+            <br />
+            <br />
+            Application development in both frontend and backend, conducting and receiving code reviews, management of Git branches, and building of common packages to be used across applications. Demonstrating app development to both clients and company management. Adapting to new technology stacks.
+          </p>
           <h3>General Assembly</h3>
           <h4>Software Engineering Fellow</h4>
           <br />
@@ -61,7 +63,7 @@ function Resume(props) {
             Completed May 2020.
           </p>
           <a
-            href="https://drive.google.com/file/d/1KMQD2Y7QqNGsV4kwTrjU5ORyvzsyKOuU/view?usp=sharing"
+            href="https://drive.google.com/file/d/1bbMfPVfu-QopXPl0E4McPp5cnTTPAsoJ/view?usp=sharing"
             target="_blank"
             rel="noreferrer"
             alt="link-to-resume"
